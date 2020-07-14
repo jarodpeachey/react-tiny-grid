fa186e235af751e238fa81917a9a4d58ab91e328
Jarod Peachey <jwpeachey107@aol.com>
2020-07-14 13:25:03 -0400

    More tes

--- packages/react-tiny-grid-demo/src/components/Hero.js
+++ packages/react-tiny-grid-demo/src/components/Hero.js
@@ -19,8 +19,24 @@ const Hero = () => (
       <p style={{ position: 'relative', zIndex: 99 }} className="subheading">
         A grid system for React built to simplify the quirks of Flexbox.
       </p>
-      <pre style={{ maxWidth: 600, padding: '42px 16px 16px 16px', position: 'relative' }}>
-        <span style={{color: '#ffffff90', fontSize: 14, position: 'absolute', top: 16, left: 16}}>index.js</span>
+      <pre
+        style={{
+          maxWidth: 600,
+          padding: '42px 16px 16px 16px',
+          position: 'relative',
+        }}
+      >
+        <span
+          style={{
+            color: '#ffffff90',
+            fontSize: 14,
+            position: 'absolute',
+            top: 16,
+            left: 16,
+          }}
+        >
+          index.js
+        </span>
         {'<Row>'}
         <br />
         {'  <Column />'}
@@ -31,6 +47,7 @@ const Hero = () => (
       </pre>
       <a
         className="button mr-2 mt-2"
+        style={{ display: 'block' }}
         href="https://github.com/jarodpeachey/react-tiny-grid"
       >
         Github
